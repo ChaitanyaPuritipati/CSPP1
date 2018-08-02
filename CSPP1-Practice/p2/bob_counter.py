e64dc9138fa1cebfca17da50735b68a95b22d233
ChaitanyaPuritipati <chaitanya.manipal@msitprogram.net>
2018-08-02 20:40:50 +0530

    " -> 5 of 5 passed. pylint: 10.00/10 "

---
+++ CSPP1-Practice/p2/bob_counter.py
@@ -0,0 +1,23 @@
+'''
+Author:Puritipati Chaitanya Prasad Reddy
+Date:2-8-2018
+'''
+#program to count no of bobs
+
+def main():
+    '''
+    Function checking presence of substring
+    '''
+    s_1 = input()
+    # the input string is in s
+    # remove pass and start your code here
+    end = len(s_1)
+    ctr = "bob"
+    stringcount = 0
+    for i in range(0, end):
+        if s_1[i:i+3] == ctr:
+            stringcount = stringcount + 1
+    print(stringcount)
+
+if __name__ == "__main__":
+    main()
