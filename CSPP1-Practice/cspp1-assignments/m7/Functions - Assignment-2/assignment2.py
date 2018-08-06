f7862438b9d6b7d5346be1ce7c20035f9042e88d
ChaitanyaPuritipati <chaitanya.manipal@msitprogram.net>
2018-08-06 21:29:56 +0530

    " -> 10 of 10 passed. pylint: 8.40/10 "

--- CSPP1-Practice/cspp1-assignments/m7/Functions - Assignment-2/assignment2.py
+++ CSPP1-Practice/cspp1-assignments/m7/Functions - Assignment-2/assignment2.py
@@ -12,7 +12,7 @@ def paying_debt(balance_num, annual_interest_rate, guess_num):
         mir_num = annual_interest_rate/12      
         mub_num = balance_temp - guess_num
         balance_temp = mub_num + (mir_num*mub_num)
-        i += 1
+        i = i+1
     return balance_temp
 
 def paying_debt_in_year(balance_num, annual_interest_rate):
@@ -24,9 +24,7 @@ def paying_debt_in_year(balance_num, annual_interest_rate):
     step_val = 1
     guess_num = 0.0
     while paying_debt(balance_temp, annual_interest_rate, guess_num*10) >= aprroximation_val:      
-        guess_num += step_val
-         
-    
+        guess_num = guess_num + step_val
     round_num = round(guess_num)
     return "Lowest Payment: "+str(round_num*10)
 def main():
@@ -35,7 +33,7 @@ def main():
     '''
     input_num = input()
     input_num = input_num.split(' ')
-    data = list(map(float, input_num))
+    input_num = list(map(float, input_num))
     print(paying_debt_in_year(input_num[0], input_num[1]))
     
 if __name__ == "__main__":
