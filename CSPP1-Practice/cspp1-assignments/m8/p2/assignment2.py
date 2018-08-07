b04029ae391e07c35b3e437714a3bb3cba05d2bf
ChaitanyaPuritipati <chaitanya.manipal@msitprogram.net>
2018-08-07 14:54:33 +0530

    " -> 10 of 10 passed. pylint: 7.78/10 "

---
+++ CSPP1-Practice/cspp1-assignments/m8/p2/assignment2.py
@@ -0,0 +1,22 @@
+'''
+Author: Puritipati Chaitanya Prasad Reddy
+Date: 7-8-2018
+'''
+# Exercise: Assignment-2
+def sum_of_digits(n_num):
+    '''
+    n is positive Integer
+    returns: a positive integer, the sum of digits of n.
+    '''
+    if n_num ==0:
+        return 0
+    return (n_num%10) + sum_of_digits(n_num//10)
+def main():
+    '''
+    Main function starts here
+    '''
+    input_num = input()
+    print(sum_of_digits(int(input_num)))
+if __name__ == "__main__":
+    main()
+
