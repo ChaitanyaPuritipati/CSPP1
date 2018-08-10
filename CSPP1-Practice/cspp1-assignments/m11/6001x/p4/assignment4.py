2818b0c2f1e35ca0ef2d1b260db11d9aa92e7054
ChaitanyaPuritipati <chaitanya.manipal@msitprogram.net>
2018-08-10 18:26:22 +0530

    " -> 5 of 5 passed. pylint: 10.00/10 "

--- CSPP1-Practice/cspp1-assignments/m11/6001x/p4/assignment4.py
+++ CSPP1-Practice/cspp1-assignments/m11/6001x/p4/assignment4.py
@@ -4,9 +4,8 @@ Date: 10-8-2018
 '''
 #Exercise: Assignment-4
 def calculate_handlen(test_hand):
-    """ 
+    """
     Returns the length (number of letters) in the current hand.
-    
     hand: dictionary (string int)
     returns: integer
     """
@@ -19,12 +18,12 @@ def main():
     Main function starts here
     '''
     n_num = input()
-    a_dict={}
+    a_dict = {}
     i_num = 0
     while i_num < (int(n_num)):
         input_data = input()
         l_list = input_data.split()
-        a_dict[l_list[0]]=int(l_list[1])
+        a_dict[l_list[0]] = int(l_list[1])
         i_num = i_num + 1
     print(calculate_handlen(a_dict))
 if __name__ == "__main__":
