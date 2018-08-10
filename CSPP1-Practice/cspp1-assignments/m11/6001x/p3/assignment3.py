9b66883440f5a13c1c3ebafe55e3a396ea480593
ChaitanyaPuritipati <chaitanya.manipal@msitprogram.net>
2018-08-10 18:02:38 +0530

    " -> 3 of 3 passed. pylint: 6.67/10 "

--- CSPP1-Practice/cspp1-assignments/m11/6001x/p3/assignment3.py
+++ CSPP1-Practice/cspp1-assignments/m11/6001x/p3/assignment3.py
@@ -1,12 +1,9 @@
-# Assignment-3
 '''
-At this point, we have written code to generate a random hand and display that hand to the user. We can also ask the user for a word (Python's input) and score the word (using your getWordScore). However, at this point we have not written any code to verify that a word given by a player obeys the rules of the game. A valid word is in the word list; and it is composed entirely of letters from the current hand. Implement the isValidWord function.
-
-Testing: Make sure the test_isValidWord tests pass. In addition, you will want to test your implementation by calling it multiple times on the same hand - what should the correct behavior be? Additionally, the empty string ('') is not a valid word - if you code this function correctly, you shouldn't need an additional check for this condition.
-
-Fill in the code for isValidWord in ps4a.py and be sure you've passed the appropriate tests in test_ps4a.py before pasting your function definition here.
+Author: Puritipati Chaitanya Prasad Reddy
+Date: 10-8-2018
 '''
-
+#Assignment-3
+#Module_11 
 def is_valid_word(test_word, test_hand, test_wordlist):
     """
     Returns True if word is in the wordList and is entirely
@@ -23,12 +20,12 @@ def is_valid_word(test_word, test_hand, test_wordlist):
         if i_char in test_hand:
             count_word = count_word + 1
     if count_word != len(test_word):
-        return False        
+        return False
     ln_wordlist = len(test_wordlist)
     for i_num  in range(ln_wordlist):
         if test_word in test_wordlist[i_num]:
             return True
-    return False        
+    return False
 def main():
     '''
     Main function starts here
