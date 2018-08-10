24ac6854700e24c48076add48c2fc29ec41be9ec
ChaitanyaPuritipati <chaitanya.manipal@msitprogram.net>
2018-08-10 18:25:39 +0530

    " -> 5 of 5 passed. pylint: 7.06/10 "

--- CSPP1-Practice/cspp1-assignments/m11/6001x/p4/assignment4.py
+++ CSPP1-Practice/cspp1-assignments/m11/6001x/p4/assignment4.py
@@ -15,10 +15,13 @@ def calculate_handlen(test_hand):
         counter_val = counter_val + test_hand[i_char]
     return counter_val
 def main():
-    n=input()
+    '''
+    Main function starts here
+    '''
+    n_num = input()
     a_dict={}
     i_num = 0
-    while i_num < (int(n)) :
+    while i_num < (int(n_num)):
         input_data = input()
         l_list = input_data.split()
         a_dict[l_list[0]]=int(l_list[1])
