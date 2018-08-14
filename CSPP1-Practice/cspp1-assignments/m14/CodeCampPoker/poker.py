f12a8d734cbc3b4a7322a5fbd909f68890c09bbf
ChaitanyaPuritipati <chaitanya.manipal@msitprogram.net>
2018-08-14 11:59:05 +0530

    " -> 0 of 3 passed. pylint: 8.15/10 "

--- CSPP1-Practice/cspp1-assignments/m14/CodeCampPoker/poker.py
+++ CSPP1-Practice/cspp1-assignments/m14/CodeCampPoker/poker.py
@@ -21,7 +21,7 @@ def is_straight(hand):
     print(value_list)
     for i in range(len(hand)):
         test_string.append(FACE_VALUES[hand[i][0]])
-    print(test_string)    
+    print(test_string)
     test_string.sort()
     if test_string[0] == 2 and test_string[4] == 14:
         test_string = ['A', '2', '3', '4', '5']
@@ -29,8 +29,8 @@ def is_straight(hand):
     else:
         j =0
         #print(test_string)
-        for i in test_string:
-            test_string[j] = keys_list[value_list.index(i)]
+        for k in test_string:
+            test_string[j] = keys_list[value_list.index(k)]
             j = j + 1
         #print(test_string)    
         ''.join(test_string)
