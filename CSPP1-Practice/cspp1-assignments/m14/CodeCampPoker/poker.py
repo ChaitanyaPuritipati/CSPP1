c5092f5ff46fd252cd72c118ff714d9882b44cc1
ChaitanyaPuritipati <chaitanya.manipal@msitprogram.net>
2018-08-14 12:05:21 +0530

    " -> 0 of 3 passed. pylint: 8.08/10 "

--- CSPP1-Practice/cspp1-assignments/m14/CodeCampPoker/poker.py
+++ CSPP1-Practice/cspp1-assignments/m14/CodeCampPoker/poker.py
@@ -17,15 +17,15 @@ def is_straight(hand):
     test_string = []
     value_list = list(FACE_VALUES.values())
     keys_list = list(FACE_VALUES.keys())
-    print(keys_list)
-    print(value_list)
+    #print(keys_list)
+    #print(value_list)
     for i in range(len(hand)):
         test_string.append(FACE_VALUES[hand[i][0]])
     print(test_string)
     test_string.sort()
     if test_string[0] == 2 and test_string[4] == 14:
         test_string = ['A', '2', '3', '4', '5']
-        ''.join(test_string)
+        test_string = ''.join(str(x) for x in test_string)
     else:
         j =0
         #print(test_string)
@@ -33,8 +33,8 @@ def is_straight(hand):
             test_string[j] = keys_list[value_list.index(k)]
             j = j + 1
         #print(test_string)    
-        ''.join(test_string)
-    print(test_string)    
+        test_string =''.join(str(x) for x in test_string)
+    print(test_string)
     if test_string in "A23456789TJQKA":
         return True
     return False
