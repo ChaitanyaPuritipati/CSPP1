1e205c1396047dc6d7db6e205ef32beb2fb5015c
ChaitanyaPuritipati <chaitanya.manipal@msitprogram.net>
2018-08-15 13:58:18 +0530

    " -> 0 of 8 passed. pylint: 7.82/10 "

--- CSPP1-Practice/cspp1-assignments/m14/CodeCampPoker/poker.py
+++ CSPP1-Practice/cspp1-assignments/m14/CodeCampPoker/poker.py
@@ -89,6 +89,7 @@ def full_house(hand):
     if len(test_kind) == 2:
         count_list =[]
         for i in test_kind:
+            print(i)
             count_list.append(hand.count(str(i)))
         print(count_list)
         if 3 in count_list:
