a999e27ebf051c3e73634ed65437a62363f1d4f2
ChaitanyaPuritipati <chaitanya.manipal@msitprogram.net>
2018-08-17 11:46:12 +0530

    " -> 0 of 3 passed. pylint: 8.14/10 "

--- CSPP1-Practice/cspp1-assignments/m16/CodeCampDocumentDistance/document_distance.py
+++ CSPP1-Practice/cspp1-assignments/m16/CodeCampDocumentDistance/document_distance.py
@@ -49,7 +49,7 @@ def similarity(dict1, dict2):
             common_dict[i] = [1] 
     for j in dict2:
         if j not in dict1:
-            common_dict[j] = [0]
+            common_dict[j][0] = [0]
         elif j in common_dict:
             common_dict[j][1] = common_dict[j][1] + 1
         else:
