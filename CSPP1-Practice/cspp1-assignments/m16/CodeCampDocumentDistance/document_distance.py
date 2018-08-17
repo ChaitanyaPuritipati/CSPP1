33e605b4e48c24e3a6dd04c7689120ca45ca8391
ChaitanyaPuritipati <chaitanya.manipal@msitprogram.net>
2018-08-17 08:56:32 +0530

    " -> 0 of 3 passed. pylint: 8.00/10 "

---
+++ CSPP1-Practice/cspp1-assignments/m16/CodeCampDocumentDistance/document_distance.py
@@ -0,0 +1,31 @@
+'''
+    Document Distance - A detailed description is given in the PDF
+'''
+
+def similarity(dict1, dict2):
+    '''
+        Compute the document distance as given in the PDF
+    '''
+    pass
+
+def load_stopwords(filename):
+    '''
+        loads stop words from a file and returns a dictionary
+    '''
+    stopwords = {}
+    with open(filename, 'r') as filename:
+        for line in filename:
+            stopwords[line.strip()] = 0
+    return stopwords
+
+def main():
+    '''
+        take two inputs and call the similarity function
+    '''
+    input1 = input()
+    input2 = input()
+    print(input1, input2)
+    print(similarity(input1, input2))
+
+if __name__ == '__main__':
+    main()
