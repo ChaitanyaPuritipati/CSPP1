550ce89c9b6f8ce86b68c19e9b12831fa228844e
ChaitanyaPuritipati <chaitanya.manipal@msitprogram.net>
2018-08-17 11:09:33 +0530

    " -> 0 of 3 passed. pylint: 7.67/10 "

--- CSPP1-Practice/cspp1-assignments/m16/CodeCampDocumentDistance/document_distance.py
+++ CSPP1-Practice/cspp1-assignments/m16/CodeCampDocumentDistance/document_distance.py
@@ -37,13 +37,12 @@ def similarity(dict1, dict2):
                 dict2[i][j] = dict2[i][j].split("'")
                 dict2[i][j] = ''.join(e for e in dict2[i][j])            
     stop_words = load_stopwords("stopwords.txt")
+    dict1_new = []
     for i in range(len(dict1)):
-        dict1_new = []
         for j in range(len(dict1[i])):
             print(dict1[i][j])
             if dict1[i][j] not in stop_words:
                 dict1_new.append(dict1[i][j])
-                print(dict1_new,"new..............")     
     print(dict1)
     dict1 = dict1_new
     print(dict1)         
