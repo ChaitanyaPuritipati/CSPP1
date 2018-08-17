ae2bc76b8fb71ac12506db82bde9b38cb1ac5915
ChaitanyaPuritipati <chaitanya.manipal@msitprogram.net>
2018-08-17 10:32:12 +0530

    " -> 0 of 3 passed. pylint: 7.41/10 "

--- CSPP1-Practice/cspp1-assignments/m16/CodeCampDocumentDistance/document_distance.py
+++ CSPP1-Practice/cspp1-assignments/m16/CodeCampDocumentDistance/document_distance.py
@@ -28,7 +28,7 @@ def similarity(dict1, dict2):
         for j in dict1[i]:
             j = j.strip(",.!@#$%^&*?")
             if "'" in j:
-                print("true, j")
+                print("true", j)
             print(j)
     #dict1 = dict1.split(",")
     #dict2 = dict2.split(",")
