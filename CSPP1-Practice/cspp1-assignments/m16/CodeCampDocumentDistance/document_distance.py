9275cf04ac2b4e41f5e12fa4b3ae9169475e99c2
ChaitanyaPuritipati <chaitanya.manipal@msitprogram.net>
2018-08-17 12:24:19 +0530

    " -> 0 of 3 passed. pylint: 0/0 "

--- CSPP1-Practice/cspp1-assignments/m16/CodeCampDocumentDistance/document_distance.py
+++ CSPP1-Practice/cspp1-assignments/m16/CodeCampDocumentDistance/document_distance.py
@@ -1,6 +1,20 @@
 '''
     Document Distance - A detailed description is given in the PDF
 '''
+import math
+def distance_calculator(common_dict):
+    numerator = 0
+    sqr_1 = 0
+    sqr_2 = 0
+    for k in common_dict:
+        numerator = numerator + (common_dict[k][0])*(common_dict[k][1])
+        sqr_1 = sqr_1 + (common_dict[k][0])**2
+        sqr_2 = sqr_2 + (common_dict[k][1])**2
+    sqr_1 = math.sqrt(sqr_1)
+    sqr_2 = math.sqrt(sqr_2)
+    denominator = (sqr_1)*(sqr_2)
+    output = numerator/denominator
+    return output
 
 def similarity(dict1, dict2):
     '''
@@ -34,16 +48,16 @@ def similarity(dict1, dict2):
         for j in range(len(dict1[i])):
             #print(dict1[i][j])
             if dict1[i][j] not in stop_words:
-                if dict1[i][j] != ' ':
-                    dict1_new.append(dict1[i][j])
+            #    if dict1[i][j] != ' ':
+            #        dict1_new.append(dict1[i][j])
     dict1 = dict1_new
     dict2_new = []
     for i in range(len(dict2)):
         for j in range(len(dict2[i])):
             #print(dict2[i][j])
-            if dict2[i][j] not in stop_words:
-                if dict2[i][j] != ' ':
-                    dict2_new.append(dict2[i][j])
+            #if dict2[i][j] not in stop_words:
+            #    if dict2[i][j] != ' ':
+            #        dict2_new.append(dict2[i][j])
     dict2 = dict2_new
     common_dict = {}
     for i in dict1:
@@ -56,10 +70,10 @@ def similarity(dict1, dict2):
             common_dict[j] = [0, 1]
         elif j in common_dict:
             common_dict[j][1] = common_dict[j][1] + 1
-    common_dict.pop('',None)
-    print(list(common_dict.keys()))     
-    print(len(common_dict))
-
+    common_dict.pop('', None)
+    #print(list(common_dict.keys()))     
+    #print(len(common_dict))
+    print(distance_calculator(common_dict))
 def load_stopwords(filename):
     '''
         loads stop words from a file and returns a dictionary
