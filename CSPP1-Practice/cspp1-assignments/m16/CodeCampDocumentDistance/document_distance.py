2451bee285b3cc279928b18a56a9d987723e871a
ChaitanyaPuritipati <chaitanya.manipal@msitprogram.net>
2018-08-17 12:25:47 +0530

    " -> 0 of 3 passed. pylint: 7.91/10 "

--- CSPP1-Practice/cspp1-assignments/m16/CodeCampDocumentDistance/document_distance.py
+++ CSPP1-Practice/cspp1-assignments/m16/CodeCampDocumentDistance/document_distance.py
@@ -72,7 +72,7 @@ def similarity(dict1, dict2):
             common_dict[j][1] = common_dict[j][1] + 1
     common_dict.pop('', None)
     #print(list(common_dict.keys()))     
-    #print(len(common_dict))
+    print(len(common_dict))
     print(distance_calculator(common_dict))
 def load_stopwords(filename):
     '''
