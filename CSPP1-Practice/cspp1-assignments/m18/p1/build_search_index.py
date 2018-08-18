08bf6b2deb864a115d5c52a6fdad2769ce045d5d
ChaitanyaPuritipati <chaitanya.manipal@msitprogram.net>
2018-08-18 09:36:01 +0530

    " -> 0 of 1 passed. pylint: 0/0 "

--- CSPP1-Practice/cspp1-assignments/m18/p1/build_search_index.py
+++ CSPP1-Practice/cspp1-assignments/m18/p1/build_search_index.py
@@ -41,7 +41,8 @@ def word_list(text):
     '''
     for line in range(len(text)):
     	text[line] = text[line].lower().split()
-    print(text)	
+        text[line] = (e for e in text[line] if e.isaplha())
+        print(text[line])    	
 def build_search_index(docs):
     '''
         Process the docs step by step as given below
