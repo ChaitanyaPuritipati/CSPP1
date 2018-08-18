722860d3a1e007cfd7cc7775bdbe8e1be160cba3
ChaitanyaPuritipati <chaitanya.manipal@msitprogram.net>
2018-08-18 09:42:01 +0530

    " -> 0 of 1 passed. pylint: 0/0 "

--- CSPP1-Practice/cspp1-assignments/m18/p1/build_search_index.py
+++ CSPP1-Practice/cspp1-assignments/m18/p1/build_search_index.py
@@ -41,7 +41,8 @@ def word_list(text):
     '''
     for line in range(len(text)):
         text[line] = text[line].lower().split()
-    
+        text[line] = (for e in text[line] e.strip("!@#$%^&*?.,;:"))
+        print(text[line])
     print(text)       
 def build_search_index(docs):
     '''
