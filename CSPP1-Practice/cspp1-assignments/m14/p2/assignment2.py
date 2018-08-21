b0cd7e7d5f9f214507dd1224caba071fed625932
ChaitanyaPuritipati <chaitanya.manipal@msitprogram.net>
2018-08-21 23:14:48 +0530

    " -> 5 of 5 passed. pylint: 8.98/10 "

--- CSPP1-Practice/cspp1-assignments/m14/p2/assignment2.py
+++ CSPP1-Practice/cspp1-assignments/m14/p2/assignment2.py
@@ -40,7 +40,7 @@ def load_words(file_name):
 
 WORDLIST_FILENAME = 'words.txt'
 
-class Message(object):
+class Message():
     ''' Grader's Implementation of Message Object '''
 
     ### DO NOT MODIFY THIS METHOD ###
