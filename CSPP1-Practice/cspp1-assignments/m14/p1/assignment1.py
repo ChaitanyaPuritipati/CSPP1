495382061af88a0b8f70f9ae5a3de92abecac412
ChaitanyaPuritipati <chaitanya.manipal@msitprogram.net>
2018-08-21 23:21:19 +0530

    " -> 4 of 5 passed. pylint: 8.72/10 "

--- CSPP1-Practice/cspp1-assignments/m14/p1/assignment1.py
+++ CSPP1-Practice/cspp1-assignments/m14/p1/assignment1.py
@@ -100,7 +100,7 @@ class Message():
         character shifted down the alphabet by the input shift. The dictionary
         should have 52 keys of all the uppercase letters and all the lowercase
         letters only.
-        shift (integer): the amount by which to shift every letter of the 
+        shift (integer): the amount by which to shift every letter of the
         alphabet. 0 <= shift < 26
         Returns: a dictionary mapping a letter (string) to 
                  another letter (string). 
