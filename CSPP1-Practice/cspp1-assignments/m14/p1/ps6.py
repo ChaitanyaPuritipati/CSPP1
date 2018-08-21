7de929dd1c51bfefee12555382b5e241536aff30
ChaitanyaPuritipati <chaitanya.manipal@msitprogram.net>
2018-08-21 22:18:23 +0530

    " -> 0 of 5 passed. pylint: 1.96/10 "

--- CSPP1-Practice/cspp1-assignments/m14/p1/ps6.py
+++ CSPP1-Practice/cspp1-assignments/m14/p1/ps6.py
@@ -92,7 +92,7 @@ class Message(object):
         The dictionary maps every uppercase and lowercase letter to a
         character shifted down the alphabet by the input shift. The dictionary
         should have 52 keys of all the uppercase letters and all the lowercase
-        letters only.        
+        letters only.
         
         shift (integer): the amount by which to shift every letter of the 
         alphabet. 0 <= shift < 26
@@ -100,8 +100,7 @@ class Message(object):
         Returns: a dictionary mapping a letter (string) to 
                  another letter (string). 
         '''
-        pass #delete this line and replace with your code here
-
+        print(list(string.ascii_lowercase))
     def apply_shift(self, shift):
         '''
         Applies the Caesar Cipher to self.message_text with the input shift.
