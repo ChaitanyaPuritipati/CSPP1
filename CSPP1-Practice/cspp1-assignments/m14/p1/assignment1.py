909a7a352722edd395b6a2104c940a00d87ffcec
ChaitanyaPuritipati <chaitanya.manipal@msitprogram.net>
2018-08-21 22:39:18 +0530

    " -> 0 of 5 passed. pylint: 2.35/10 "

--- CSPP1-Practice/cspp1-assignments/m14/p1/assignment1.py
+++ CSPP1-Practice/cspp1-assignments/m14/p1/assignment1.py
@@ -132,7 +132,7 @@ class Message(object):
         Returns: the message text (string) in which every character is shifted
              down the alphabet by the input shift
         '''
-        print(self)
+        print(self.message_text)
         shift_dict = Message.build_shift_dict(shift)
         
 
