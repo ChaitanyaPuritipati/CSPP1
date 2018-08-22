caad566a2b5b1f1397c3077bd42ccb5d81b3fb47
ChaitanyaPuritipati <chaitanya.manipal@msitprogram.net>
2018-08-22 22:58:20 +0530

    " -> 3 of 3 passed. pylint: 9.41/10 "

--- CSPP1-Practice/cspp1-assignments/m15/P1/assignment1.py
+++ CSPP1-Practice/cspp1-assignments/m15/P1/assignment1.py
@@ -58,7 +58,7 @@ def get_story_string():
 
 WORDLIST_FILENAME = 'words.txt'
 
-class Message(object):
+class Message():
     ''' Message object '''
     ### DO NOT MODIFY THIS METHOD ###
     def __init__(self, text):
