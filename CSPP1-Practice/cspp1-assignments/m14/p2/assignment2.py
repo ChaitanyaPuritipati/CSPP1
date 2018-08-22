5cd2f891f66422118616de9fe2788f8722dbb087
ChaitanyaPuritipati <chaitanya.manipal@msitprogram.net>
2018-08-22 09:42:53 +0530

    " -> 5 of 5 passed. pylint: 3.33/10 "

--- CSPP1-Practice/cspp1-assignments/m14/p2/assignment2.py
+++ CSPP1-Practice/cspp1-assignments/m14/p2/assignment2.py
@@ -400,7 +400,7 @@ class PlaintextMessage(Message):
         Hint: consider using the parent class constructor so less 
         code is repeated
         '''
-        self.message_text = text
+        Message.__init__(self, text)
         self.shift = shift
     def get_shift(self):
         '''
