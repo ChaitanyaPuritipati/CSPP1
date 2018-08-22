4199bc9d0ba6f85023dcf604fa1326ee861d07e7
ChaitanyaPuritipati <chaitanya.manipal@msitprogram.net>
2018-08-22 20:15:50 +0530

    " -> 3 of 3 passed. pylint: 8.37/10 "

--- CSPP1-Practice/cspp1-assignments/m15/P1/assignment1.py
+++ CSPP1-Practice/cspp1-assignments/m15/P1/assignment1.py
@@ -275,10 +275,10 @@ class CiphertextMessage(Message):
             if self.length == count:
                 self.list.append(self.shift)
             if self.length == len(self.teststring):
-                output_string = Message.apply_shift(self, self.shift)
+                output_string = Message.apply_shift(self, 26-(self.shift))
                 return(self.shift, output_string)
             self.shift += 1
-        return (min(self.list), Message.apply_shift(self, min(self.list)))
+        return (min(self.list), Message.apply_shift(self, 26-min(self.list)))
 ### DO NOT MODIFY THIS METHOD ###
 def main():
     ''' This method is provided to handle testcases'''
