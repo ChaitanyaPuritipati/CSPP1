2078fd56944583dcb28b36dbfd458f4d97624d1a
ChaitanyaPuritipati <chaitanya.manipal@msitprogram.net>
2018-08-23 12:19:17 +0530

    " -> 2 of 8 passed. pylint: 5.83/10 "

--- CSPP1-Practice/cspp1-assignments/m16/Matrix Operations/matrix_operations.py
+++ CSPP1-Practice/cspp1-assignments/m16/Matrix Operations/matrix_operations.py
@@ -23,7 +23,7 @@ def mult_matrix(m1, m2, dimension_matrix1, dimension_matrix2):
         for test_ele in range(len(m2_val[0])):
             ele_list = []
             for col_ele in range(len(m2_val)):
-                ele_list.append((m1_val[row_ele][col_ele])*(m2_val[col_ele][row_ele]))
+                ele_list.append((m1_val[test_ele][col_ele])*(m2_val[col_ele][test_ele]))
             print(ele_list)
     print(ele_list)            
     return multi_matrix
