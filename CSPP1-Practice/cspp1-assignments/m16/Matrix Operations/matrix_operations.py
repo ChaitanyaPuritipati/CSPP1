1e21f6e1127ccf84de84dbc18638a623fa5df47e
ChaitanyaPuritipati <chaitanya.manipal@msitprogram.net>
2018-08-23 19:02:35 +0530

    " -> 8 of 8 passed. pylint: 6.23/10 "

--- CSPP1-Practice/cspp1-assignments/m16/Matrix Operations/matrix_operations.py
+++ CSPP1-Practice/cspp1-assignments/m16/Matrix Operations/matrix_operations.py
@@ -18,7 +18,7 @@ def mult_matrix(m1, m2, dimension_matrix1, dimension_matrix2):
     m1_val = type_conv(m1)
     m2_val = type_conv(m2)
     multi_matrix = []
-    #multi_matrix = [[sum(a * b for a, b in zip(m1_val_row, m2_val_col)) for m2_val_col in zip(*m2_val)]for m1_val_row in m1_val] 
+    #multi_matrix = [[sum(a * b for a, b in zip(m1_val_row, m2_val_col)) for m2_val_col in zip(*m2_val)]for m1_val_row in m1_val]
     for row_ele in range(len(m1_val)):
         row_list = []
         for test_ele in range(len(m2_val[0])):
