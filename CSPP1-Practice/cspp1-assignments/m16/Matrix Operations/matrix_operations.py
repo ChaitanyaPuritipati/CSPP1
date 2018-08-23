f0fc1c931460f866fed17c71c4e1e62d4bd3c318
ChaitanyaPuritipati <chaitanya.manipal@msitprogram.net>
2018-08-23 09:32:18 +0530

    " -> 0 of 8 passed. pylint: 2.35/10 "

--- CSPP1-Practice/cspp1-assignments/m16/Matrix Operations/matrix_operations.py
+++ CSPP1-Practice/cspp1-assignments/m16/Matrix Operations/matrix_operations.py
@@ -30,7 +30,7 @@ def read_matrix(dimension_matrix):
     columns = int(dimension_matrix[1])
     row_dict = {}
     for row in range(rows):
-        for colums in range(columns):
+        for columns in range(columns):
             input()
     return row_dict        
 def main():
