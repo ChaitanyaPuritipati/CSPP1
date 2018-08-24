2c89fc385175e1d68f127167d54a36788e8ce41f
ChaitanyaPuritipati <chaitanya.manipal@msitprogram.net>
2018-08-24 10:03:37 +0530

    " -> 0 of 14 passed. pylint: 0/0 "

--- CSPP1-Practice/cspp1-assignments/m21/Tic Tac Toe/tictactoe.py
+++ CSPP1-Practice/cspp1-assignments/m21/Tic Tac Toe/tictactoe.py
@@ -20,44 +20,49 @@ class Tictactoe(object):
     def get_board(self):
         return self.board
     def check_rows(self):
-        if set(self.board[0]) == {'X'} or set(self.board[1]) == {'X'} or set(self.board[2]) == {'X'} or set(self.board[0]) == {'O'} or set(self.board[1]) == {'O'} or set(self.board[2]) == {'O'}:
-            return True
-        return False
+        if set(self.board[0]) == {'x'} or set(self.board[1]) == {'x'} or set(self.board[2]) == {'x'} 
+            return 'x'
+        if set(self.board[0]) == {'o'} or set(self.board[1]) == {'o'} or set(self.board[2]) == {'o'}:
+            return 'o'
     def check_columns(self):
-        if set([self.board[0][0],self.board[1][0], self.board[2][0]]) == {'X'} \
-        or set([self.board[0][1],self.board[1][1],self.board[2][1]]) == {'X'} \
-        or set([self.board[0][2],self.board[1][2], self.board[2][2]]) == {'X'} \
-        or set([self.board[0][0],self.board[1][0], self.board[2][0]]) == {'O'} \
-        or set([self.board[0][1],self.board[1][1], self.board[2][1]]) == {'O'} \
-        or set([self.board[0][2],self.board[1][2], self.board[2][2]]) == {'O'}:
-            return True
-        return False
+        if set([self.board[0][0],self.board[1][0], self.board[2][0]]) == {'x'} \
+        or set([self.board[0][1],self.board[1][1],self.board[2][1]]) == {'x'} \
+        or set([self.board[0][2],self.board[1][2], self.board[2][2]]) == {'x'}:
+            return 'x'
+        
+        if set([self.board[0][0],self.board[1][0], self.board[2][0]]) == {'o'} \
+        or set([self.board[0][1],self.board[1][1], self.board[2][1]]) == {'o'} \
+        or set([self.board[0][2],self.board[1][2], self.board[2][2]]) == {'o'}:
+            return 'y'
     def check_diagonals(self):
-        if set([self.board[0][0], self.board[1][1], self.board[2][2]]) == {'X'}\
-        or set([self.board[0][2], self.board[1][1], self.board[2][0]]) == {'X'}\
-        or set([self.board[0][0], self.board[1][1], self.board[2][2]]) == {'O'}\
-        or set([self.board[0][2], self.board[1][1], self.board[2][0]]) == {'O'}:
-            return True
-        return False
+        if set([self.board[0][0], self.board[1][1], self.board[2][2]]) == {'x'}\
+        or set([self.board[0][2], self.board[1][1], self.board[2][0]]) == {'x'}:
+            return 'x'
+        if set([self.board[0][0], self.board[1][1], self.board[2][2]]) == {'o'}\
+        or set([self.board[0][2], self.board[1][1], self.board[2][0]]) == {'o'}:
+            return 'y'    
     def board_check(self):
+        check = 0
         for ele in self.board:
-            if '_' in ele:
-                return True
-        return False                   
+        	check = check + ele.count('.') + ele.count('x') + ele.count('o')
+        if check != 9:
+            return 1	
+                #return True
+        return 0                   
 
 #board = [['_','_','_'],['_','_','_'],['_','_','_']]
 board = []
 for i in range(3):
 	board.append(input().split()) 
-print(board)
-#game = Tictactoe(board)
+game = Tictactoe(board)
+if game.board_check() == 1:
+    return "invalid input"
 #def displayboard(board):
     #for i in board:
         #for j in i:
             #print(j," ", end="")
-        #print() 
+        #print()
 #displayboard(board) 
-# print(board)
 #while True:
     #player_1_input = input("Player 1 input enter here: ").split()
     #game.update_board(int(player_1_input[0]), int(player_1_input[1]), "X", 1)
