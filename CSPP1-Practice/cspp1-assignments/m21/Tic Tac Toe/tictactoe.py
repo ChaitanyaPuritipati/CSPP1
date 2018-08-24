720349696f934d21df8ab2df69151f6117b95424
ChaitanyaPuritipati <chaitanya.manipal@msitprogram.net>
2018-08-24 10:05:22 +0530

    " -> 0 of 14 passed. pylint: -1.63/10 "

--- CSPP1-Practice/cspp1-assignments/m21/Tic Tac Toe/tictactoe.py
+++ CSPP1-Practice/cspp1-assignments/m21/Tic Tac Toe/tictactoe.py
@@ -51,48 +51,49 @@ class Tictactoe(object):
         return 0                   
 
 #board = [['_','_','_'],['_','_','_'],['_','_','_']]
-board = []
-for i in range(3):
-	board.append(input().split()) 
-game = Tictactoe(board)
-if game.board_check() == 1:
-    print("invalid input")
-#def displayboard(board):
-    #for i in board:
-        #for j in i:
-            #print(j," ", end="")
-        #print()
-#displayboard(board) 
-#while True:
-    #player_1_input = input("Player 1 input enter here: ").split()
-    #game.update_board(int(player_1_input[0]), int(player_1_input[1]), "X", 1)
-    #board_print = game.get_board()
-    #displayboard(board_print)
-    #print(board_print)
-    #if game.check_rows() == True or game.check_columns() == True or game.check_diagonals() == True:
-        #print("Player 1 wins")
-        #break
-    #if game.board_check() == False:
-        #print("Game Over! Ran out of chances")
-        #reset_input = input("Enter Y to create a new board and start the game again: ")
-        #if reset_input == 'Y' or reset_input == 'y':
-            #game.__reset__()
-        #else:
-            #print("Thanks for playing the game!")
-            #break  
-    #player_2_input = input("Player 2 input enter here: ").split()
-    #game.update_board(int(player_2_input[0]), int(player_2_input[1]), "O", 2)
-    #board_print = game.get_board()
-    #print(board_print)
-    #displayboard(board_print)
-    #if game.check_rows() == True or game.check_columns() == True or game.check_diagonals() == True:
-        #print("Player 2 wins")
-        #break
-    #if game.board_check() == False:
-        #print("Game Over! Ran out of chances")
-        #reset_input = input("Enter Y to create a new board and start the game again: ")
-        #if reset_input == 'Y' or reset_input == 'y':
-            #game.__reset__()
-        #else:
-            #print("Thanks for Playing the game!")
-            #break
+def main():
+	board = []
+	for i in range(3):
+		board.append(input().split()) 
+	game = Tictactoe(board)
+	if game.board_check() == 1:
+	    return "invalid input"
+	#def displayboard(board):
+	    #for i in board:
+	        #for j in i:
+	            #print(j," ", end="")
+	        #print()
+	#displayboard(board) 
+	#while True:
+	    #player_1_input = input("Player 1 input enter here: ").split()
+	    #game.update_board(int(player_1_input[0]), int(player_1_input[1]), "X", 1)
+	    #board_print = game.get_board()
+	    #displayboard(board_print)
+	    #print(board_print)
+	    #if game.check_rows() == True or game.check_columns() == True or game.check_diagonals() == True:
+	        #print("Player 1 wins")
+	        #break
+	    #if game.board_check() == False:
+	        #print("Game Over! Ran out of chances")
+	        #reset_input = input("Enter Y to create a new board and start the game again: ")
+	        #if reset_input == 'Y' or reset_input == 'y':
+	            #game.__reset__()
+	        #else:
+	            #print("Thanks for playing the game!")
+	            #break  
+	    #player_2_input = input("Player 2 input enter here: ").split()
+	    #game.update_board(int(player_2_input[0]), int(player_2_input[1]), "O", 2)
+	    #board_print = game.get_board()
+	    #print(board_print)
+	    #displayboard(board_print)
+	    #if game.check_rows() == True or game.check_columns() == True or game.check_diagonals() == True:
+	        #print("Player 2 wins")
+	        #break
+	    #if game.board_check() == False:
+	        #print("Game Over! Ran out of chances")
+	        #reset_input = input("Enter Y to create a new board and start the game again: ")
+	        #if reset_input == 'Y' or reset_input == 'y':
+	            #game.__reset__()
+	        #else:
+	            #print("Thanks for Playing the game!")
+	            #break
