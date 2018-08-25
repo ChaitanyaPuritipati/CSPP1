8f9d5184a7afea539c168910ee23ddb90c3a9c9a
ChaitanyaPuritipati <chaitanya.manipal@msitprogram.net>
2018-08-25 11:08:56 +0530

    " -> 0 of 4 passed. pylint: 5.56/10 "

--- CSPP1-Practice/cspp1-assignments/m22/assignment3/tokenize.py
+++ CSPP1-Practice/cspp1-assignments/m22/assignment3/tokenize.py
@@ -4,19 +4,19 @@ each word
 '''
 
 # #def tokenize(string):
-# 	output_dict = {}
+#   output_dict = {}
 
 #     for every_ele in string:
-#     	if every_ele in output_dict:
-#     		output_dict[every_ele] += 1
-#     	else:
-#     	    output_dict[every_ele] = 1	
+#       if every_ele in output_dict:
+#           output_dict[every_ele] += 1
+#       else:
+#           output_dict[every_ele] = 1  
 #     return output_dict
 def main():
     num_lines = int(input())
     for i in range(num_lines):
-    	input_str = input()
-    	input_str = (input_str.split())
+        input_str = input()
+        input_str = (input_str.split())
         input_str = (' '.join(e for e in input_str if e.isalpha())).split()
     print(input_str)    
 if __name__ == '__main__':
