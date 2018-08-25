34a6a45c5b340502218f9ce021de07bb8c333ce2
ChaitanyaPuritipati <chaitanya.manipal@msitprogram.net>
2018-08-25 10:56:18 +0530

    " -> 3 of 3 passed. pylint: 10.00/10 "

--- CSPP1-Practice/cspp1-assignments/m22/assignment2/clean_input.py
+++ CSPP1-Practice/cspp1-assignments/m22/assignment2/clean_input.py
@@ -1,17 +1,26 @@
 '''
-Write a function to clean up a given string by removing the special characters and retain 
+Author: P.Chaitanya Prasad Reddy
+Roll no: 20186018
+Date: 25-08-2018
+CSPP1 EXAM WEEK 4
+-----------------------------------------------------------------------------------------
+Write a function to clean up a given string by removing the special characters and retain
 alphabets in both upper and lower case and numbers.
 '''
 
 def clean_string(string):
+    '''
+    Function to clean a string
+    '''
     output_str = ""
-    for e in string:
-        if e not in '!@#$%^&*()., ':
-            output_str += e
-    return output_str       
-
-
+    for every_char in string:
+        if every_char not in '!@#$%^&*()., ':
+            output_str += every_char
+    return output_str
 def main():
+    '''
+    Main function starts here
+    '''
     string = input()
     print(clean_string(string))
 
