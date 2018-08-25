34a86c4b0f2b22c29a234980132275f1ad0229fd
ChaitanyaPuritipati <chaitanya.manipal@msitprogram.net>
2018-08-25 11:19:47 +0530

    " -> 4 of 4 passed. pylint: 10.00/10 "

--- CSPP1-Practice/cspp1-assignments/m22/assignment3/tokenize.py
+++ CSPP1-Practice/cspp1-assignments/m22/assignment3/tokenize.py
@@ -4,20 +4,27 @@ each word
 '''
 
 def tokenize(string, output_dict):
+    '''
+    Tokenize function starts here
+    '''
     string = (string.split())
     for every_ele in string:
         every_ele = every_ele.strip('",;.')
         if every_ele in output_dict:
             output_dict[every_ele] += 1
         else:
-            output_dict[every_ele] = 1  
+            output_dict[every_ele] = 1
     return output_dict
 def main():
+    '''
+    Main Function starts here
+    '''
     output_dict = {}
     num_lines = int(input())
-    for i in range(num_lines):
+    for iter_num in range(num_lines):
         input_str = input()
         output_dict = tokenize(input_str, output_dict)
+        iter_num += 1
     print(output_dict)
 if __name__ == '__main__':
     main()
