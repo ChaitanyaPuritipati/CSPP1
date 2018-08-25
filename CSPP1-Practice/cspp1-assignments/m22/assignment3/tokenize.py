d337ec8eb5281595e1a26faa20b94380b3c44038
ChaitanyaPuritipati <chaitanya.manipal@msitprogram.net>
2018-08-25 11:08:48 +0530

    " -> 0 of 4 passed. pylint: 0/0 "

--- CSPP1-Practice/cspp1-assignments/m22/assignment3/tokenize.py
+++ CSPP1-Practice/cspp1-assignments/m22/assignment3/tokenize.py
@@ -3,15 +3,15 @@ Write a function to tokenize a given string and return a dictionary with the fre
 each word
 '''
 
-def tokenize(string):
-	output_dict = {}
+# #def tokenize(string):
+# 	output_dict = {}
 
-    for every_ele in string:
-    	if every_ele in output_dict:
-    		output_dict[every_ele] += 1
-    	else:
-    	    output_dict[every_ele] = 1	
-    return output_dict
+#     for every_ele in string:
+#     	if every_ele in output_dict:
+#     		output_dict[every_ele] += 1
+#     	else:
+#     	    output_dict[every_ele] = 1	
+#     return output_dict
 def main():
     num_lines = int(input())
     for i in range(num_lines):
