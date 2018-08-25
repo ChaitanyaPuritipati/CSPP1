1d2eb2bf489c2074aad6ab50268253215add1bd8
ChaitanyaPuritipati <chaitanya.manipal@msitprogram.net>
2018-08-25 23:25:24 +0530

    " -> 15 of 26 passed. pylint: 9.42/10 "

--- CSPP1-Practice/cspp1-assignments/m22/Check Sudoku/check_sudoku.py
+++ CSPP1-Practice/cspp1-assignments/m22/Check Sudoku/check_sudoku.py
@@ -19,7 +19,6 @@ def check_grid(sudoku):
         while i < (len(sudoku[0])-2):
             new_rows.append(every_row[i:i+3])
             i = i+3
-    #print(new_rows)
     for i in range(3):
         j = i
         while j <= i+2*(len(sudoku)):
@@ -40,15 +39,12 @@ def check_col(sudoku):
         for every_col in range(len(sudoku[0])):
             col_set.add(sudoku[every_col][every_row])
         if len(col_set) != len(sudoku):
-            print("False")
             return False
-    print("true")        
     return True
 def check_row(sudoku):
     '''
     function to check rows
     '''
-    print(len(sudoku))
     for every_row in sudoku:
         if len(set(every_row)) != len(sudoku):
             return False
@@ -58,16 +54,18 @@ def check_sudoku(sudoku):
         Your solution goes here. You may add other helper functions as needed.
         The function has to return True for a valid sudoku grid and false otherwise
     '''
+    for every_row in sudoku:
+        for i in every_row:
+            if i not in "1234567890":
+                print("executed")
+                return False
     if check_row(sudoku) is False:
         return False
     if check_col(sudoku) is False:
         return False
     if check_grid(sudoku) is False:
         return False    
-    for every_row in sudoku:
-        for i in every_row:
-            if i not in "1234567890":
-                return False
+
     return True
 
 def main():
